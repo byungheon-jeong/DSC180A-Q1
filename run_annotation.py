4454b75ac2fd0c912ccbef185a2cb552726d23e8
byungheon-jeong <45370147+byungheon-jeong@users.noreply.github.com>
2021-11-20 14:12:28 -0800

    Add Log functionality

--- run_annotation.py
+++ run_annotation.py
@@ -67,6 +67,21 @@ def testPixelMask(img,paths,viewer):
     
     return ice_coordinates,non_ice_coordinates
 
+def createLog(directory):
+    with open(os.path.join(directory, "imagelog.data"), "wb+") as image_log:
+        try:
+            imageList = pickle.load(image_log)
+        except (EOFError) as e:
+            imageList = []
+            pickle.dump(imageList, file=image_log)
+        return imageList
+
+
+def updateLog(imageList,log_path):
+    with open(os.path.join(log_path), "wb+") as image_log:
+        pickle.dump(imageList, file=image_log)
+
+
 def test():
     try:
         test_image_path = input("Input the image to test:\n")
@@ -89,33 +104,33 @@ def test():
             print("Label the image")
     return test_mask
 
-def createLog(image_log):
-    try:
-        imageList = pickle.load(image_log)
-    except (EOFError) as e:
-        imageList = []
-        pickle.dump(imageList, file=image_log)
 
 def main():
-    directory = input("Enter Directory:\n")
-    with open(os.path.join(directory, "imagelog.data"), "wb+") as image_log:
-        annotated_list = createLog(image_log)
+    # directory = input("Enter Directory:\n")
+    directory = r"C:\Users\marke\Documents\DSC180A-Q1\ee_data"
+    annotated_list = createLog(directory)
 
     for root, dirs, files in os.walk(directory):            
-        for file in files:
-            image_file = os.path.splitext(file)[1]
-            if image_file == ".tif" and image_file not in annotated_list:
-                print(image_file)
-                # viewer,img = runNapari(image_file)
-                # while True:
-                #     input("Press Enter after Labeling")
-                #     try:    
-                #         paths = getPolygonMasks(viewer)
-                #         test_mask = getPixelMask(img,paths)
-                #         testPixelMask(img, paths, viewer)
-                #         break
-                #     except:
-                #         print("Label the image")
+        for image_file in files:
+            image_path = os.path.join(directory, image_file)
+            print(image_path)
+            if os.path.splitext(image_path)[1] == ".tif" and image_file not in annotated_list:
+                    viewer,img = runNapari(image_path)
+                    while True:
+                        response = input("Press Enter after Labeling or input \"SKIP\" in order to skip image:\n")
+                        if response == "SKIP":
+                            viewer.close()
+                            break
+                        try:    
+                            paths = getPolygonMasks(viewer)
+                            test_mask = getPixelMask(img,paths)
+                            testPixelMask(img, paths, viewer)
+                            annotated_list.append(image_file)
+                            updateLog(annotated_list, image_path)
+                            break
+                        except Exception as e:
+                            print(f"{e} \nLabel the image")
+                    
                 # return test_mask
 
     
